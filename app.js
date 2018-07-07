946e7d64b3ddae7b7b5cad88c98ab3482bf9804e
eswaribala <Parameswaribala@gmail.com>
2018-07-07 12:54:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -22,9 +22,33 @@ console.log("Messsage=",$message);
 console.log("Info=",info);
 
 */
+//when window loads
+window.onload=init;
+
+function init()
+{
+   cityData=new Array("Karur","Trichy","Coimbatore","Madurai");
+   dataListRef=document.querySelector("#cityList")
+   //add city to array
+   cityData.push("Kanyakumari");
+   cityData.unshift("Chennai");
+   // read array
+   for(cityName in cityData)
+      {
+        console.log(cityData[cityName]);
+        elem=document.createElement("option");
+        text=document.createTextNode(cityData[cityName]);
+        elem.appendChild(text);
+        dataListRef.appendChild(elem);
+
+      }
+
+}
+
 
 intRate=0;
 tenure=0;
+city="";
 //create a function
 function irCaution()
 {
@@ -45,21 +69,52 @@ function tenureCaution()
          alert("Tenure can be 3 or 5 or 7",tenure);
 }
 
+function readCity(value)
+{
+   city=value; 
+  
+
+}
+
+
+
 function calculate()
 {
+     //Access result area
+    resultRef=document.querySelector("#result");
+    
+    while(resultRef.hasChildNodes())
+    {
+        resultRef.removeChild(resultRef.firstChild);
+        //console.log("yes");
+    }
+    
+    dob=document.querySelector('#dob').valueAsDate;
+    birthYear=1900+dob.getYear();
+    currentYear=1900+new Date().getYear();
+    age=currentYear-birthYear;
+    if(age>21)
+    {
     //alert("Form submitted..."+tenure);
     loanAmt=document.querySelector("#loanAmount").value;
     interest=Math.round(loanAmt*(intRate/100),0);
     //convert string to int
     totalAmt=parseInt(loanAmt)+interest;
-    emi=totalAmt/(tenure*12);
+    emi=Math.round(totalAmt/(tenure*12),0);
     console.log("Monthly EMI="+emi);
-    //Access result area
-    resultRef=document.querySelector("#result");
+   
+    paraElement=document.createElement("p");
+    textNode=document.createTextNode("City Name="+city);
+    paraElement.appendChild(textNode);
+    resultRef.appendChild(paraElement); 
+
     paraElement=document.createElement("p");
     textNode=document.createTextNode("MonthlyEMI="+emi);
     paraElement.appendChild(textNode);
     resultRef.appendChild(paraElement); 
+    }
+    else
+      alert("Not Eligible for loan");
     return false; 
 
 
