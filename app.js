61214ae8e56c2134ae46abc2e2c3950e7dd8cd02
eswaribala <Parameswaribala@gmail.com>
2018-07-07 14:15:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -60,6 +60,12 @@ function irCaution()
 
 }
 
+function autoTab(field1, len, field2) {
+	if (document.getElementById(field1).value.length == len) {
+		document.getElementById(field2).focus();
+		}
+}
+
 function tenureCaution()
 {
     //local variable to global
