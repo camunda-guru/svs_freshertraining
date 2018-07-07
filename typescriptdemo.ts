0e9f0f56a29904b3abc300115f6e9b08011d89b6
eswaribala <Parameswaribala@gmail.com>
2018-07-07 17:50:10 +0530

    Add files via upload

---
+++ typescriptdemo.ts
@@ -0,0 +1,21 @@
+//data types
+var firstName:string;
+var lastName:string;
+var dob:Date;
+var active:boolean;
+var income:number;
+//assign values
+firstName="Arun";
+lastName="Kumar";
+dob=new Date(1970,12,2);
+active=true;
+income=5000000;
+//display results
+console.log("First Name=",firstName);
+console.log("Last Name",lastName);
+console.log("DOB",dob);
+
+
+
+
+
